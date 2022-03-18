f64fcdbb657e380a99df6f1278ec6d33fc458f7d
Sebastian2k21 <seba0071@interia.pl>
2022-03-18 09:38:33 +0100

    Tworzenie class , metod ...

---
+++ Lab_1/Lab_2/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+https://dirask.com/posts/WSEI-2021-2022-lato-labN-1-ISN-Programowanie-obiektowe-labs-pa3ek1
+*/
+namespace Lab_2
+{
+    class Person
+    {
+        protected string name;
+
+        protected int age;
+
+        public string Name { get => name; set => name = value; }
+
+        public int Age { get => age; set => age = value; }
+
+
+
+
+
+        public Person(string PersonName, int PersonAge)
+        {
+            Name = PersonName;
+            Age = PersonAge;
+        }
+
+        public override bool Equals(object other)
+        {
+            Person person = other as Person;
+            if (person == null) return false;
+            return person.Name == Name && person.Age == Age;
+            
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+
+    }
+}
+
---
+++ Lab_1/Lab_2/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Student:Person
+    {
+        protected string group;
+        protected List<Task> tasks;
+        public string Group { get => group; set => group = value ; }
+
+
+        public Student (string PersonName,int PersonAge , string PersonGroup): base( PersonName, PersonAge)
+        {
+            group = PersonGroup;
+        }
+
+        public Student(string PersonName, int PersonAge, string PersonGroup, List<Task> tasks1 ) : base(PersonName, PersonAge)
+        {
+            tasks = tasks1;
+        }
+
+        public void AddTask (string taskName, TaskStat taskStatus)
+        {
+            var task = new Task(taskName, taskStatus);
+            tasks.Add(task);
+        }
+
+        public void  RemoveTask(int index) => tasks.RemoveAt(index);
+
+        public void UpdateTask(int index , TaskStat taskStatus)
+        
+
+        
+    }
+}
